01453b0a338c8f5d39208e6f8fc3da2638d69ce5
Sebastian Hohoł <103772116+MerkucjoGrande@users.noreply.github.com>
2023-02-06 18:11:02 +0100

    Update Almost-random-Number-generator.cpp

--- Almost-random-Number-generator.cpp
+++ Almost-random-Number-generator.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
+#include <string>
+
+int main()
+{
+
+std::string start;
+int num;
+std::cout << "Almost random number generator" << std::endl;
+do
+{
+    std::cout << "Generate a number ranging from 1-10" << std::endl;
+    std::cout << "Press any key to start or 'q' to quit" << std::endl;
+    srand(time(0));
+    num = rand() % 10 + 1;
+    std::getline(std::cin, start);
+    std::cout << "Your random number is: " << num << std::endl;
+
+} while (start != "q");
+return 0;
+}
+
 
