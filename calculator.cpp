15a29fc224683804303acbac3f9dd8736de9858a
Sebastian Hohoł <103772116+MerkucjoGrande@users.noreply.github.com>
2023-02-08 23:55:33 +0100

    Update calculator.cpp
    When prompted for a choice 1/4 program won't crassh after chosing a letter for instance. Furthermore balance can't fall below zero, you can't scam the bank anymore

--- calculator.cpp
+++ calculator.cpp
@@ -1,79 +1,71 @@
 #include <iostream>
-#include <cmath>
+#include <limits>
 
 int main()
 {
-
-    char op;
-    double num1;
-    double num2;
-    double result;
-
-    std::cout << "$$$Calculator$$$" << std::endl;
+    int choice = 0;
+    double balance = 0;
+    double add;
+    double sub;
     do
     {
-        std::cout << "Choose an operand + - * / or press q to quit" << std::endl;
-        std::cin >> op;
-        if (op=='q')
+        std::cout << "Choose an option" << std::endl;
+        std::cout << "1.View Balance" << std::endl;
+        std::cout << "2.Deposit Money" << std::endl;
+        std::cout << "3.Withdraw Money" << std::endl;
+        std::cout << "4.Exit" << std::endl;
+        std::cin >> choice;
+        if (!std::cin)
         {
-            std::cout << "Quitting program";
-            break;
-        }
-        
-        std::cout << "Choose number 1" << std::endl;
-        std::cin >> num1;
-        if (std::cin.fail() || op == 'q')
-        {   
-            std::cout << "Quitting program";
-            break;
-        }
-        if (std::cin.fail())
-        {
-            std::cout << "Invalid input, try again!" << std::endl;
+            std::cout << "Invalid input, try again!"<< std::endl;
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
         }
-        std::cout << "Chose number 2" << std::endl;
-        std::cin >> num2;
-        if (std::cin.fail() || op == 'q')
+        if (choice == 1)
         {
-            std::cout << "Quitting program"; 
-            break;
+            std::cout << "You Chose to View Balance" << std::endl;
+            std::cout << "Your Balance Is: " << balance << std::endl;
         }
-        if (std::cin.fail())
+        else if (choice == 2)
         {
-            std::cout << "Invalid input, try again!" << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "You Chose to Deposit Money" << std::endl;
+            std::cout << "How much would you like to deposit?" << std::endl;
+            std::cin >> add;
+            if (add < 0)
+            {
+                std::cout << "That's not a valid amount:\n";
+            }
+            else
+            {
+                balance = balance + add;
+                std::cout << "Your new Balance is: " << balance << std::endl;
+            }
         }
+        else if (choice == 3)
+        {
+            std::cout << "You Chose to Withdraw Money" << std::endl;
+            std::cout << "How much would you like to withdraw?" << std::endl;
+            std::cin >> sub;
+            if (sub < 0)
+            {
+                std::cout << "That's not a valid amount:\n";
+            }
+            else if (balance - sub < 0)
+            {
+                std::cout << "Insufficient balance. Please deposit money first.\n";
+            }
+            else
+            {
+                balance = balance - sub;
+                std::cout << "Your new Balance is: " << balance << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Thank you, bye!\n";
+        }
+    } while (choice != 4);
 
-        switch (op)
-        {
-        case '+':
-            std::cout << "You chose addition" << std::endl;
-            std::cout << "The result of addition is: " << num1 + num2 << std::endl;
-            break;
-        case '-':
-            std::cout << "You chose subtraction" << std::endl;
-            std::cout << "The result of subtraction is: " << num1 - num2 << std::endl;
-            break;
-        case '*':
-            std::cout << "You chose multiplication" << std::endl;
-            std::cout << "The result of multiplication is: " << num1 * num2 << std::endl;
-            break;
-        case '/':
-        if (num2==0)
-        {
-            break;
-        }
-            std::cout << "You chose division" << std::endl;
-
-            std::cout << "The result of division is: " << num1 / num2 << std::endl;
-            break;
-        default:
-            std::cout << "Wrong input!" << std::endl;
-            break;
-        }
-    } while (op != 'q');
     return 0;
 }
